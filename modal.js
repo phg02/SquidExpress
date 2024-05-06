1f135edb9ea1f973169f564006265a1467b717f7
Phuong Dao <s4029319@rmit.edu.vn>
2024-05-06 23:31:52 +0700

    :3333

--- modal.js
+++ modal.js
@@ -1,28 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.see-more-btn').forEach(function(button) {
-        let postContent = button.parentElement.querySelector('.post-title');
-      
-        if (postContent.scrollWidth > postContent.clientWidth) {
-            button.style.display = 'block';
-        } 
-        else {
-            button.style.display = 'none';
-        }
-      
-        button.addEventListener('click', function() {
-            let clickedPostContent = this.parentElement.querySelector('.post-title');
-            clickedPostContent.classList.toggle('expanded');
-
-            if (clickedPostContent.classList.contains('expanded')) {
-                button.textContent = 'See less';
-            } 
-            else {
-                button.textContent = 'See more';
-            }
-        });
-    });
-});
-
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
